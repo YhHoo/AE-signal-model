48f6672947e7c721ad372a10c4a62cb1919c8e30
Hoo Yu Heng <hooyuheng@gmail.com>
2018-06-07 19:34:07 +0800

    writing AE data 30-5-18

--- src/experiment_dataset/dataset_experiment_30_5_2018.py
+++ src/experiment_dataset/dataset_experiment_30_5_2018.py
@@ -1,2 +1,14 @@
 
 path = 'F:/Experiment_30_5_2018/test1_-2,-1,22,23m/PLB, Hammer/2m/PLB'
+
+# for Pencil Lead Break data, the sensor position are (-2, -1, 22, 23)m
+class AcousticEmissionDataSet_30_5_2018:
+    def __init__(self, drive):
+        self.drive = drive + ':/'
+        self.path_0m_plb = self.drive + 'Experiment_30_5_2018/test1_-2,-1,22,23m/PLB, Hammer/0m/PLB/'
+        self.path_2m_plb = self.drive + 'Experiment_30_5_2018/test1_-2,-1,22,23m/PLB, Hammer/2m/PLB/'
+        self.path_4m_plb = self.drive + 'Experiment_30_5_2018/test1_-2,-1,22,23m/PLB, Hammer/4m/PLB/'
+        self.path_6m_plb = self.drive + 'Experiment_30_5_2018/test1_-2,-1,22,23m/PLB, Hammer/6m/PLB/'
+
+    def PLB_4_sensor(self):
+        
