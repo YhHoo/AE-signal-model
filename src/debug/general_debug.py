10927a96898953f8986a4c39321ced6af9790b27
Hoo Yu Heng <hooyuheng@gmail.com>
2018-06-11 16:29:25 +0800

    paused

--- src/debug/general_debug.py
+++ src/debug/general_debug.py
@@ -1,5 +1,7 @@
 import numpy as np
-import mayavi.mlab as mlb
+import matplotlib.pyplot as plt
+from scipy import signal
+# import mayavi.mlab as mlb
 
 
 # three_dim_visualizer()
@@ -13,39 +15,36 @@ print(data_3d.shape)
 # print(data_3d.shape[1])
 # print(data_3d[0].shape[0])
 
-s = np.arange(0, 100, 1).reshape((5, 20))
-x = np.arange(0, 20, 1)
-y = np.arange(0, 5, 1)
-print(s)
-print(x)
-print(y)
-mlb.barchart(y, x, s)
-mlb.imshow()
+# s = np.arange(0, 100, 1).reshape((5, 20))
+# x = np.arange(0, 20, 1)
+# y = np.arange(0, 5, 1)
+# print(s)
+# print(x)
+# print(y)
+# mlb.barchart(y, x, s)
+# mlb.imshow()
+sig = np.repeat([0., 1., 0., 1], 100)
+win = signal.hann(50)
+print(sum(win))
+mat1 = np.array([3, 9, 2, 1, 0, 0, 0, 0])
+mat2 = np.array([0, 3, 9, 2, 1, 0, 0, 0])
 
-# data_2d = np.array([[1, 2],
-#                     [3, 4] ,
-#                     [2, 5]])
-# data_2d = data_2d.astype(dtype='float32')
-
-# mat1 = np.array([1, 0, 3, 3, 2, 1, 0, 0]).reshape((1, 8))
-# mat2 = np.array([3, 1, 0, 3, 3, 2, 1, 0]).reshape((1, 8))
-#
 # mat3 = np.array([0, 0, 1, 0, 1, 0, 0, 0]).reshape((1, 8))
 # mat4 = np.array([0, 0, 0, 1, 0, 1, 0, 0]).reshape((1, 8))
-#
+plt.plot(sig)
+plt.plot(win, marker='x')
+plt.show()
+
+
 # ori_signal = np.concatenate((mat1, mat3), axis=0)
 # lag_signal = np.concatenate((mat2, mat4), axis=0)
 # print(ori_signal.shape)
 
-# cor = np.correlate(mat1, mat2, 'full')
+# cor = np.correlate(mat2, mat1, 'full')
 # print('USING NUMPY CORRELATE---------------')
 # print(cor)
 # print(cor.shape)
 # print(np.argmax(cor))
-
-# cor = correlate(ori_signal, lag_signal, 'full')
-# print('USING SCIPY CORRELATE---------------')
-# print(cor)
-# print(cor.shape)
-# print(np.argmax(cor))
+# plt.plot(cor)
+# plt.show()
 
