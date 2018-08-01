48e8852fdc8563a6d224fdf01a2de14b2b55d785
Yu Heng <hooyuheng@gmail.com>
2018-08-01 11:16:03 +0800

    to PC

--- src/scripts/dataset_experiment_2018_7_13_model_vis_script(PLB).py
+++ src/scripts/dataset_experiment_2018_7_13_model_vis_script(PLB).py
@@ -19,7 +19,8 @@ train_x, train_y, test_x, test_y = break_into_train_test(input=dataset,
                                                          label=label,
                                                          num_classes=num_classes,
                                                          train_split=0.7,
-                                                         verbose=True)
+                                                         verbose=True,
+                                                         shuffled_each_class=False)
 # reshape to satisfy conv2d input shape
 _, _, test_x, _ = reshape_3d_to_4d_tocategorical(train_x, train_y, test_x, test_y,
                                                  fourth_dim=1,
@@ -31,43 +32,24 @@ _, _, test_x, _ = reshape_3d_to_4d_tocategorical(train_x, train_y, test_x, test_
 model = model_loader(model_name='PLB_2018_7_13_Classification_CNN[33k]_take0')
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-# print(model.layers)
+activation = get_activations(model, model_inputs=test_x, print_shape_only=True)
 
-# for layer in model.layers:
-#     print(layer)
-#     print(layer.output)
+print(len(activation))
 
-activation = get_activations(model, model_inputs=test_x, print_shape_only=True, layer_name='conv2d_1')
 
-sample_no = 0
-# for all samples
-for a in activation[0]:
-    # put all fmap into 1 list
-    fmap_list = [a[:, :, i] for i in range(a.shape[2])]
-    label = 'Conv2d_1 - Sample_no ={}, Label=[{}m]'.format(sample_no, test_y[sample_no])
-    fig = plot_multiple_horizontal_heatmap(fmap_list,
-                                           title=label,
-                                           subplot_title='F_map')
-    # fig.savefig()
-    plt.show()
-    # # for all feature map
-    # for i in range(a.shape[2]):
-    #     plt.imshow(a[:, :, i], interpolation='None', cmap='jet')
-    #     label = 'Sample_no ={}, Label=[{}m], fmap_no={}'.format(sample_no, test_y[sample_no], i)
-    #     save_filename = save_dir + label
-    #     plt.title(label)
-    #     plt.savefig(save_filename)
-    #     plt.close('all')
-    sample_no += 1
-
-# for i in range(8):
-#     f = fmap[:, :, i]
-#     print(f.shape)
-#     fig = three_dim_visualizer(x_axis=np.arange(0, f.shape[1], 1),
-#                                y_axis=np.arange(0, f.shape[0], 1),
-#                                zxx=f,
-#                                output='2d')
-#     plt.show()
+# sample_no = 0
+# # for all samples
+# for a in activation[0]:
+#     # put all fmap into 1 list
+#     fmap_list = [a[:, :, i] for i in range(a.shape[2])]
+#     label = 'Maxpooling2d_3 - Sample_no ={}, Label=[{}m]'.format(sample_no, test_y[sample_no])
+#     save_filename = save_dir + label
+#     fig = plot_multiple_horizontal_heatmap(fmap_list,
+#                                            title=label,
+#                                            subplot_title='F_map')
+#     fig.savefig(save_filename)
 #     plt.close('all')
+#     sample_no += 1
+
 
 
