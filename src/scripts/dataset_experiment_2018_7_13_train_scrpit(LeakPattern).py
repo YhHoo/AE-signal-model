028b9d10cdc3f784180990867a8d28dd601ac1ec
Yu Heng <hooyuheng@gmail.com>
2018-08-21 12:02:20 +0800

    to laptop

--- src/scripts/dataset_experiment_2018_7_13_train_scrpit(LeakPattern).py
+++ src/scripts/dataset_experiment_2018_7_13_train_scrpit(LeakPattern).py
@@ -10,18 +10,18 @@ from src.experiment_dataset.dataset_experiment_2018_7_13 import AcousticEmission
 
 # Config
 num_classes = 11
-nn_input_shape = 8
+nn_input_shape = 60
 
 # reading data ---------------------------------------------------------------------------------------------------------
 data = AcousticEmissionDataSet_13_7_2018(drive='F')
 dataset, label = data.leak_1bar_in_cwt_xcor_maxpoints_vector(dataset_no=2,
-                                                             f_range_to_keep=(0, 8),
+                                                             f_range_to_keep=(40, 100),
                                                              class_to_keep='all')
 # data pre-processing
 train_x, train_y, test_x, test_y = break_into_train_test(input=dataset,
                                                          label=label,
                                                          num_classes=num_classes,
-                                                         train_split=1,
+                                                         train_split=0.7,
                                                          verbose=True,
                                                          shuffled_each_class=True)
 
@@ -31,39 +31,41 @@ test_y_cat = to_categorical(test_y, num_classes=num_classes)
 
 
 # training -------------------------------------------------------------------------------------------------------------
+model = fc_leak_1bar_max_vec_v2(input_shape=nn_input_shape, num_classes=num_classes)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+model_logger = ModelLogger(model, model_name='fc_leak_1bar_max_vec_v2')
+history = model.fit(x=train_x,
+                    y=train_y_cat,
+                    batch_size=600,
+                    validation_data=(test_x, test_y_cat),
+                    epochs=100,
+                    verbose=2)
+model_logger.learning_curve(history=history, save=False, show=True)
 
-def create_model(optimizer='adam'):
-    model = fc_leak_1bar_max_vec_v2(input_shape=nn_input_shape, num_classes=num_classes)
-    # optimizer
-    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['acc'])
-
-    return model
-
-
-seed = 7
-np.random.seed(seed)
-model = KerasClassifier(build_fn=create_model, verbose=0, epochs=100)
-
-# grid search param
-batch_size = [200, 500, 600, 700, 800, 900, 1000, 2000]
-optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
-param_grid = dict(batch_size=batch_size, optimizer=optimizer)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
-grid_result = grid.fit(train_x, train_y_cat)
-
-# model_logger = ModelLogger(model, model_name='fc_leak_1bar_max_vec_v2')
-# history = model.fit(x=train_x,
-#                     y=train_y_cat,
-#                     batch_size=500,
-#                     validation_data=(test_x, test_y_cat),
-#                     epochs=60,
-#                     verbose=2)
-# model_logger.learning_curve(history=history, save=False, show=True)
-
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+# Grid search hyperparam -----------------------------------------------------------------------------------------------
+# def create_model(optimizer='adam'):
+#     model = fc_leak_1bar_max_vec_v2(input_shape=nn_input_shape, num_classes=num_classes)
+#     # optimizer
+#     model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['acc'])
+#
+#     return model
+#
+#
+# seed = 7
+# np.random.seed(seed)
+# model = KerasClassifier(build_fn=create_model, verbose=0, epochs=100)
+#
+# # grid search param
+# batch_size = [200, 500, 600, 700, 800, 900, 1000, 2000]
+# # optimizer = ['SGD', 'RMSprop']  #, 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam'
+# param_grid = dict(batch_size=batch_size)
+# grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
+# grid_result = grid.fit(train_x, train_y_cat)
+#
+# # summarize results
+# print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+# means = grid_result.cv_results_['mean_test_score']
+# stds = grid_result.cv_results_['std_test_score']
+# params = grid_result.cv_results_['params']
+# for mean, stdev, param in zip(means, stds, params):
+#     print("%f (%f) with: %r" % (mean, stdev, param))
