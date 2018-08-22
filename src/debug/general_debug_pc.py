73301e8203f4fb3275a178e8cba92d54ba792687
Yu Heng <hooyuheng@gmail.com>
2018-08-23 02:35:19 +0800

    to laptop

--- src/debug/general_debug_pc.py
+++ src/debug/general_debug_pc.py
@@ -15,13 +15,49 @@ from os import listdir
 from keras.utils import to_categorical
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.metrics import confusion_matrix, precision_recall_fscore_support
+from sklearn.model_selection import StratifiedKFold
 # self lib
 from src.controlled_dataset.ideal_dataset import white_noise
 from src.utils.dsp_tools import spectrogram_scipy, one_dim_xcor_2d_input
 from src.experiment_dataset.dataset_experiment_2018_7_13 import AcousticEmissionDataSet_13_7_2018
 from src.utils.helpers import plot_heatmap_series_in_one_column, read_single_tdms, direct_to_dir, ProgressBarForLoop, \
                               break_into_train_test, ModelLogger, reshape_3d_to_4d_tocategorical
-from src.model_bank.dataset_2018_7_13_leak_model import fc_leak_1bar_max_vec_v1
+from src.model_bank.dataset_2018_7_13_leak_localize_model import fc_leak_1bar_max_vec_v1
+
+
+# Config
+num_classes = 11
+nn_input_shape = (60, )
+
+# reading data ---------------------------------------------------------------------------------------------------------
+data = AcousticEmissionDataSet_13_7_2018(drive='F')
+dataset, label = data.leak_1bar_in_cwt_xcor_maxpoints_vector(dataset_no=2,
+                                                             f_range_to_keep=(40, 100),
+                                                             class_to_keep='all')
+
+label_cat = to_categorical(label, num_classes=num_classes)
+
+print(label_cat.shape)
+
+# # data pre-processing
+# train_x, train_y, test_x, test_y = break_into_train_test(input=dataset,
+#                                                          label=label,
+#                                                          num_classes=num_classes,
+#                                                          train_split=1,
+#                                                          verbose=True,
+#                                                          shuffled_each_class=True)
+#
+
+#
+# kfold = StratifiedKFold(n_splits=2, shuffle=True, random_state=4)
+#
+# for train, test in kfold.split(dataset, label):
+#     print(train.shape)
+#     print(test.shape)
+
+
+
+# testing grid search CV of sklearn ------------------------------------------------------------------------------------
 
 # Use scikit-learn to grid search the batch size and epochs
 import numpy
@@ -31,40 +67,39 @@ from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasClassifier
 # Function to create model, required for KerasClassifier
 
-
-def create_model():
-    # create model
-    model = Sequential()
-    model.add(Dense(12, input_dim=8, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    # Compile model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
-
-
-# fix random seed for reproducibility
-seed = 7
-numpy.random.seed(seed)
-# load dataset
-dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
-# split into input (X) and output (Y) variables
-X = dataset[:, 0:8]
-Y = dataset[:, 8]
-# create model
-model = KerasClassifier(build_fn=create_model, verbose=0)
-# define the grid search parameters
-batch_size = [10, 20, 40, 60, 80, 100]
-epochs = [10, 50, 100]
-param_grid = dict(batch_size=batch_size, epochs=epochs)
-grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
-grid_result = grid.fit(X, Y)
-# summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+# def create_model():
+#     # create model
+#     model = Sequential()
+#     model.add(Dense(12, input_dim=8, activation='relu'))
+#     model.add(Dense(1, activation='sigmoid'))
+#     # Compile model
+#     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#     return model
+#
+#
+# # fix random seed for reproducibility
+# seed = 7
+# numpy.random.seed(seed)
+# # load dataset
+# dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+# # split into input (X) and output (Y) variables
+# X = dataset[:, 0:8]
+# Y = dataset[:, 8]
+# # create model
+# model = KerasClassifier(build_fn=create_model, verbose=0)
+# # define the grid search parameters
+# batch_size = [10, 20, 40, 60, 80, 100]
+# epochs = [10, 50, 100]
+# param_grid = dict(batch_size=batch_size, epochs=epochs)
+# grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
+# grid_result = grid.fit(X, Y)
+# # summarize results
+# print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+# means = grid_result.cv_results_['mean_test_score']
+# stds = grid_result.cv_results_['std_test_score']
+# params = grid_result.cv_results_['params']
+# for mean, stdev, param in zip(means, stds, params):
+#     print("%f (%f) with: %r" % (mean, stdev, param))
 
 # ------------------------------------[TESTING MAX VEC GENERATOR]-------------------------------------------------------
 # # wavelet
