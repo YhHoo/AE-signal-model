8d56a37d1fccd53af1204519a108ce841f3c8c1f
Hoo Yu Heng <hooyuheng@gmail.com>
2018-09-11 23:47:33 +0800

    to pc

--- src/experiment_dataset/dataset_experiment_2018_7_13_(LeakPatterGenScript)/parallel_run_2.py
+++ src/experiment_dataset/dataset_experiment_2018_7_13_(LeakPatterGenScript)/parallel_run_2.py
@@ -3,7 +3,7 @@ from os import listdir
 from src.experiment_dataset.dataset_experiment_2018_7_13 import AcousticEmissionDataSet_13_7_2018
 
 # sharing the total files
-folder_path = 'F:/Experiment_13_7_2018/Experiment 1/-3,-2,2,4,6,8,10,12/1 bar/Leak/'
+folder_path = 'E:/Experiment_13_7_2018/Experiment 1/-3,-2,2,4,6,8,10,12/1 bar/Leak/'
 all_file_path = [(folder_path + f) for f in listdir(folder_path) if f.endswith('.tdms')]
 all_file_path = all_file_path[19:38]
 print('FILE TO PROCESSED:')
