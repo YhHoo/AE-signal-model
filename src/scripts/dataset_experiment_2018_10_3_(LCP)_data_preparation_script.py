d1e91e0bb0ffb1bef2847486da56c2f65365de47
Hoo Yu Heng <hooyuheng@gmail.com>
2018-10-12 17:43:37 +0800

    to PC

--- src/scripts/dataset_experiment_2018_10_3_(LCP)_data_preparation_script.py
+++ src/scripts/dataset_experiment_2018_10_3_(LCP)_data_preparation_script.py
@@ -9,11 +9,10 @@ from src.utils.helpers import *
 
 # CONFIG ---------------------------------------------------------------------------------------------------------------
 # operation
-extract_lcp = False
-extract_non_lcp = True
+extract_lcp = True
+# extract_non_lcp = True
 shuffle_tdms_list = True
 max_non_lcp_sample_size = int(30e3)
-
 
 # roi
 roi_width = (int(1e3), int(5e3))
@@ -23,33 +22,33 @@ lcp_dataset_save_filename = direct_to_dir(where='result') + 'dataset_lcp_2bar_ne
 non_lcp_dataset_save_filename = direct_to_dir(where='result') + 'dataset_non_lcp_2bar_near_seg3.csv'
 
 # points offset when segment other channel, with respect to channel 2m and -2m
-# -3m
-offset_ch0 = 600
-# 4m
-offset_ch3 = 1100
-# 6m
-offset_ch4 = 2700
+# -4.5m
+offset_ch0 = 1700
+# 5m
+offset_ch3 = 1600
 # 8m
-offset_ch5 = 4000
+offset_ch4 = 3400
 # 10m
-offset_ch6 = 5300
-# 12m
-offset_ch7 = 6500
+offset_ch5 = 6000
+# # 10m
+# offset_ch6 = 5300
+# # 12m
+# offset_ch7 = 6500
 
-offset_all = [offset_ch0, 0, 0, offset_ch3, offset_ch4, offset_ch5, offset_ch6, offset_ch7]
+offset_all = [offset_ch0, 0, 0, offset_ch3, offset_ch4, offset_ch5]
 
 # READING LCP INDEXES --------------------------------------------------------------------------------------------------
 # all file name
-tdms_dir = 'F:/Experiment_13_7_2018/Experiment 1/-3,-2,2,4,6,8,10,12/2 bar/Leak/'
+tdms_dir = 'F:/Experiment_3_10_2018/-4.5, -2, 2, 5, 8, 10, 17 (leak 1bar)/'
 
 all_tdms_file = [(tdms_dir + f) for f in listdir(tdms_dir) if f.endswith('.tdms')]
-if shuffle_tdms_list:
-    random = np.random.permutation(len(all_tdms_file))
-    all_tdms_file = np.array(all_tdms_file)[random]
+# if shuffle_tdms_list:
+#     random = np.random.permutation(len(all_tdms_file))
+#     all_tdms_file = np.array(all_tdms_file)[random]
 
 # read the LCP indexes into df
-lcp_index_filename = 'F:/Experiment_13_7_2018/Experiment 1/-3,-2,2,4,6,8,10,12/LCP DATASET/' + \
-                    'lcp_index_2bar_near_segmentation3.csv'
+lcp_index_filename = 'C:/Users/YH/Desktop/hooyuheng.masterWork/LCP DATASET OCT 3 1BAR/' \
+                     'lcp_index_1bar_near_segmentation4_p0_8.csv'
 
 df_1bar_lcp = pd.read_csv(lcp_index_filename, index_col=0)
 
@@ -57,17 +56,16 @@ df_1bar_lcp = pd.read_csv(lcp_index_filename, index_col=0)
 # set up a csv headers
 header = np.arange(0, roi_width[0]+roi_width[1], 1).tolist() + ['channel']
 
-if extract_lcp:
-    # write header to csv
-    with open(lcp_dataset_save_filename, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
+# write header to csv
+with open(lcp_dataset_save_filename, 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
 
-if extract_non_lcp:
-    # write header to csv
-    with open(non_lcp_dataset_save_filename, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(header)
+# if extract_non_lcp:
+#     # write header to csv
+#     with open(non_lcp_dataset_save_filename, 'w', newline='') as f:
+#         writer = csv.writer(f)
+#         writer.writerow(header)
 
 sample_size = 0
 # for all tdms files
@@ -94,7 +92,7 @@ for foi in all_tdms_file:
     # SEGMENTING LCP ---------------------------------------------------------------------------------------------------
     if extract_lcp:
         # read all channels info into 2d array
-        lcp_channel_arr = temp_df[['ch0', 'ch1', 'ch2', 'ch3', 'ch4', 'ch5', 'ch6', 'ch7']].values
+        lcp_channel_arr = temp_df[['ch0', 'ch1', 'ch2', 'ch3', 'ch4', 'ch5']].values
 
         print('Extracting LCP --> ', lcp_index_per_file)
         # for all lcp in one file
@@ -123,55 +121,55 @@ for foi in all_tdms_file:
     # WARNING: TERMINATE THIS CODE SOMEWHERE WHEN THE SAMPLE SIZE OF THE NON-LCP IS SUFFICIENT, LOADING ALL WILL RESULT
     # IN EXTREMELY BIG CSV, WHICH IS HARD TO LOAD INTO RAM DURING TRAINING
 
-    if extract_non_lcp:
-        # only for tdms file with >1 lcp hit
-        if len(lcp_index_per_file) > 1:
-            print('Extracting non-LCP --> ', lcp_index_per_file)
-            # locating all indexed for non lcp
-            lcp_indexes_diff = np.diff(lcp_index_per_file)
-
-            non_lcp_indexes_per_file = []
-            # for all interval btw lcp index
-            for start, diff in zip(lcp_index_per_file[:-1], lcp_indexes_diff):
-                allowable_segment = diff // (roi_width[1] + roi_width[0])
-                if allowable_segment > 1:
-                    # deciding index where nonLCP is started to be taken
-                    start_index = start + roi_width[0] + roi_width[1] + offset_ch7
-
-                    all_index = [start_index] + [(start_index + i * 6000) for i in range(1, allowable_segment - 1, 1)]
-                    non_lcp_indexes_per_file.append(all_index)
-
-            # unravel list-of-list into jz list
-            non_lcp_indexes_per_file = [i for sub_list in non_lcp_indexes_per_file for i in sub_list]
-
-            # writing segmentation to csv
-            for non_lcp_indexes in non_lcp_indexes_per_file:
-                data_list = []
-                # for all channels
-                for ch in np.arange(0, 8, 1):
-                    # segment 6000 points + channel label
-                    soi = n_channel_data_near_leak[ch,
-                                                   non_lcp_indexes:
-                                                   (non_lcp_indexes+roi_width[0]+roi_width[1])].tolist() + [ch]
-                    data_list.append(soi)
-                    sample_size += 1
-
-                # save to one csv
-                with open(non_lcp_dataset_save_filename, 'a', newline='') as f:
-                    writer = csv.writer(f)
-                    for row in data_list:
-                        writer.writerow(row)
-
-            print('non_lcp appended --> ', lcp_dataset_save_filename)
-            print('non_lcp sample size: ', sample_size)
-
-            # check the sample size with limit
-            if sample_size > max_non_lcp_sample_size:
-                print('Sample size limit achieved ! Terminating the code..')
-                break
-
-        else:
-            print('Non-LCP not extracted')
+    # if extract_non_lcp:
+    #     # only for tdms file with >1 lcp hit
+    #     if len(lcp_index_per_file) > 1:
+    #         print('Extracting non-LCP --> ', lcp_index_per_file)
+    #         # locating all indexed for non lcp
+    #         lcp_indexes_diff = np.diff(lcp_index_per_file)
+    #
+    #         non_lcp_indexes_per_file = []
+    #         # for all interval btw lcp index
+    #         for start, diff in zip(lcp_index_per_file[:-1], lcp_indexes_diff):
+    #             allowable_segment = diff // (roi_width[1] + roi_width[0])
+    #             if allowable_segment > 1:
+    #                 # deciding index where nonLCP is started to be taken
+    #                 start_index = start + roi_width[0] + roi_width[1] + offset_ch7
+    #
+    #                 all_index = [start_index] + [(start_index + i * 6000) for i in range(1, allowable_segment - 1, 1)]
+    #                 non_lcp_indexes_per_file.append(all_index)
+    #
+    #         # unravel list-of-list into jz list
+    #         non_lcp_indexes_per_file = [i for sub_list in non_lcp_indexes_per_file for i in sub_list]
+    #
+    #         # writing segmentation to csv
+    #         for non_lcp_indexes in non_lcp_indexes_per_file:
+    #             data_list = []
+    #             # for all channels
+    #             for ch in np.arange(0, 8, 1):
+    #                 # segment 6000 points + channel label
+    #                 soi = n_channel_data_near_leak[ch,
+    #                                                non_lcp_indexes:
+    #                                                (non_lcp_indexes+roi_width[0]+roi_width[1])].tolist() + [ch]
+    #                 data_list.append(soi)
+    #                 sample_size += 1
+    #
+    #             # save to one csv
+    #             with open(non_lcp_dataset_save_filename, 'a', newline='') as f:
+    #                 writer = csv.writer(f)
+    #                 for row in data_list:
+    #                     writer.writerow(row)
+    #
+    #         print('non_lcp appended --> ', lcp_dataset_save_filename)
+    #         print('non_lcp sample size: ', sample_size)
+    #
+    #         # check the sample size with limit
+    #         if sample_size > max_non_lcp_sample_size:
+    #             print('Sample size limit achieved ! Terminating the code..')
+    #             break
+    #
+    #     else:
+    #         print('Non-LCP not extracted')
 
     # free up memory for tdms stored
     gc.collect()
