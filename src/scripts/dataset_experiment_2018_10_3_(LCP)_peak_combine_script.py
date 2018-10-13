ae4aadb3f54f7eeb0cdaf2a7a2b3c463ad954fad
Yu Heng <hooyuheng@gmail.com>
2018-10-13 17:18:44 +0800

    today

--- src/scripts/dataset_experiment_2018_10_3_(LCP)_peak_combine_script.py
+++ src/scripts/dataset_experiment_2018_10_3_(LCP)_peak_combine_script.py
@@ -1,22 +1,23 @@
 # self lib
 from src.utils.helpers import *
 
-filename = 'lcp_index_1bar_near_segmentation4'
+# CONFIG ---------------------------------------------------------------------------------------------------------------
 lcp_index_dir = 'C:/Users/YH/Desktop/hooyuheng.masterWork/LCP DATASET OCT 3 1BAR/'
-filename_to_save = direct_to_dir(where='result') + filename + '.csv'
-filename_list = [lcp_index_dir + filename + '_p{}.csv'.format(i) for i in range(5)]
+filename_to_save = direct_to_dir(where='result') + 'lcp_index_1bar_near_segmentation4_p0_8.csv'
+filename_to_read_list = [(lcp_index_dir + f) for f in listdir(lcp_index_dir) if f.endswith('.csv')]
 
-for f in filename_list:
+# to confirm all files involved is true
+for f in filename_to_read_list:
     print(f)
 
-
+# READ AND APPEND ------------------------------------------------------------------------------------------------------
 # jus to get the col name
-df = pd.read_csv(filename_list[0], index_col=0)
+df = pd.read_csv(filename_to_read_list[0], index_col=0)
 column_name = df.columns.values
 
 # collect data from all csv
 data_arr_list = []
-for f in filename_list:
+for f in filename_to_read_list:
     df = pd.read_csv(f, index_col=0)
     data_arr_list.append(df.values)
 
