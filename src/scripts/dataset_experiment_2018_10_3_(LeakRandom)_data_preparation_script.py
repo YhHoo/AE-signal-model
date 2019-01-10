5b77b8e4cfe77f69424c6fa5e33edd95926ab10d
Yu Heng <hooyuheng@gmail.com>
2019-01-11 02:45:43 +0800

    to c

--- src/scripts/dataset_experiment_2018_10_3_(LeakRandom)_data_preparation_script.py
+++ src/scripts/dataset_experiment_2018_10_3_(LeakRandom)_data_preparation_script.py
@@ -1,14 +1,45 @@
+import sys
+sys.path.append('C:/Users/YH/PycharmProjects/AE-signal-model')
+
 import csv
 import gc
+import argparse
 from scipy.signal import decimate
 from src.utils.helpers import *
 
 
+# argparse
+parser = argparse.ArgumentParser(description='Input some parameters.')
+parser.add_argument('--fts', metavar='FS', default=None, type=str, help='Filename to save')
+parser.add_argument('--ftr', metavar='FR', default=None, type=str, help='Filename to process')
+parser.add_argument('--cth', metavar='CH', default=None, type=int, nargs='+', help='Channel no to extract')
+parser.add_argument('--svs', metavar='S', default=None, type=int, help='sample vector size')
+parser.add_argument('--dsf', metavar='DF', default=1, type=int, help='Downsample factor')
+
+args = parser.parse_args()
+
+FILENAME_TO_SAVE = args.fts
+FOLDER_TO_READ = args.ftr
+CHANNEL_TO_EXTRACT = args.cth
+SAMPLE_VECTOR_LENGTH = args.svs
+DOWNSAMPLE_FACTOR = args.dsf
+
+print('Filename to save: ', FILENAME_TO_SAVE)
+print('Filename to process: ', FOLDER_TO_READ)
+print('Channel no to extract: ', CHANNEL_TO_EXTRACT)
+print('sample vector size: ', SAMPLE_VECTOR_LENGTH)
+print('Downsample factor: ', DOWNSAMPLE_FACTOR)
+
+
 # CONFIG
-channels_to_take = [2]  # or np.arange(7)
-sample_vector_size = 2000
+channels_to_take = CHANNEL_TO_EXTRACT
+sample_vector_size = SAMPLE_VECTOR_LENGTH
 shuffle_tdms_seq = True
-downsample_factor_by_5 = True
+downsample_factor = DOWNSAMPLE_FACTOR
+downsample_factor_status = True
+if downsample_factor is 1:
+    downsample_factor_status = False
+
 
 random_dataset_save_filename = direct_to_dir(where='result') + 'dataset_leak_random_1.5bar_[0]_ds.csv'
 
@@ -38,9 +69,9 @@ for tdms_file in all_tdms_file:
     n_channel_data = np.swapaxes(n_channel_data, 0, 1)[:-1, :]  # drop last channel, due to no sensor
 
     temp = []
-    if downsample_factor_by_5:
+    if downsample_factor_status:
         for channel in n_channel_data:
-            temp.append(decimate(x=channel, q=5))
+            temp.append(decimate(x=channel, q=downsample_factor))
         n_channel_data = np.array(temp)
         print('Dim After Downsample: ', n_channel_data.shape)
 
--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,8 +1,36 @@
 
 #!/bin/bash
 
+declare -a file_to_save=(
+                         'C:/Users/YH/PycharmProjects/AE-signal-model/result/dataset_leak_random_1.5bar_[0]_ds2.csv'
+                         'C:/Users/YH/PycharmProjects/AE-signal-model/result/dataset_noleak_random_1.5bar_[0]_ds2.csv'
+                         'C:/Users/YH/PycharmProjects/AE-signal-model/result/dataset_leak_random_1.5bar_[-4,-2,2,4,6,8,10]_ds2.csv'
+                         'C:/Users/YH/PycharmProjects/AE-signal-model/result/dataset_noleak_random_1.5bar_[-4,-2,2,4,6,8,10]_ds2.csv'
+                        )
+
+declare -a folder_to_read=(
+                           'G:/Experiment_3_1_2019/-3,-2,0,5,7,16,17/1.5 bar/Leak/Train & Val data/'
+                           'G:/Experiment_3_1_2019/-3,-2,0,5,7,16,17/1.5 bar/NoLeak/Train & Val data/'
+                           'G:/Experiment_3_1_2019/-4,-2,2,4,6,8,10/1.5 bar/Leak/Train & Val data/'
+                           'G:/Experiment_3_1_2019/-4,-2,2,4,6,8,10/1.5 bar/NoLeak/Train & Val data/'
+                          )
+
+declare -a all_even_channel=(0 1 2 3 4 5 6 7)
+
+declare -a sample_vector_size=6000
+
+declare -a downsample_factor=10
 
 
+echo ------------------------------------------------------------------------------------------- leak [0]
+python dataset_experiment_2018_10_3_\(LeakRandom\)_data_preparation_script.py --fts "${file_to_save[0]}" --ftr "${folder_to_read[0]}" --cth ${all_even_channel[2]} --svs 6000 --dsf 10
 
-echo -----------------------NOLEAK EVAL--------------------------
-python
+echo ------------------------------------------------------------------------------------------- noleak [0]
+python dataset_experiment_2018_10_3_\(LeakRandom\)_data_preparation_script.py --fts "${file_to_save[1]}" --ftr "${folder_to_read[1]}" --cth ${all_even_channel[2]} --svs 6000 --dsf 10
+
+echo ------------------------------------------------------------------------------------------- leak [-4,-2,2,4,6,8,10]
+python dataset_experiment_2018_10_3_\(LeakRandom\)_data_preparation_script.py --fts "${file_to_save[2]}" --ftr "${folder_to_read[2]}" --cth ${all_even_channel[*]} --svs 6000 --dsf 10
+
+echo ------------------------------------------------------------------------------------------- noleak [-4,-2,2,4,6,8,10]
+python dataset_experiment_2018_10_3_\(LeakRandom\)_data_preparation_script.py --fts "${file_to_save[3]}" --ftr "${folder_to_read[3]}" --cth ${all_even_channel[*]} --svs 6000 --dsf 10
+
